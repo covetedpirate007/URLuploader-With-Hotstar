a88ae9f3bdd680fc00fba1b2259c7c8d67d045bb
Shan X <65002881+covetedpirate007@users.noreply.github.com>
2021-02-11 21:29:19 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -61,9 +61,9 @@ URL | newfilename | username | password"""
 
     DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully."
 
-    CUSTOM_CAPTION_UL_FILE = "@teamxprojects
-    @moviesonlydiscussion
- @MoViezOnlYheRe"
+    CUSTOM_CAPTION_UL_FILE = "@teamxprojects @moviesonlydiscussion @MoViezOnlYheRe" 
+
+
 
 
     SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
